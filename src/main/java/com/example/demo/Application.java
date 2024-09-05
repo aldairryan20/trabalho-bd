5edea50a0765e1cb42572c169c00033c651f7134
aldairryan20@gmail.com <aldairryan20@gmail.com>
2024-09-05 16:51:56 -0300

    05/09

--- src/main/java/com/example/demo/Application.java
+++ src/main/java/com/example/demo/Application.java
@@ -10,15 +10,99 @@ public class Application {
 		SpringApplication.run(Application.class, args);
 	}
 
-	public CommandLineRunner createDatabase() {
-		return args -> {
-			var sql = "CREATE DATABASE database";
-		};
-	}
-
 	public CommandLineRunner createTables() {
 		return args -> {
-			var sql = "CREATE TABLE conta";
+			var sql0 = "CREATE TABLE tipo_conta("+
+				"id INT,"+
+				"descricao VARCHAR(45)"+
+			");";
+			var sql1 = "CREATE TABLE conta("+
+				"id INT NOTNULL PRIMARY KEY,"+
+				"saldo DOUBLE,"+
+				"limite_negativo DOUBLE,"+
+				"FOREIGN KEY (cliente_id) REFERENCES cliente(id),"+
+				"FOREIGN KEY (tipo_conta_id) REFERENCES tipo_conta(id)"+
+			");";
+			var sql2 = "CREATE TABLE pessoa("+
+				"id INT,"+
+				"nome VARCHAR(45),"+
+				"cpf VARCHAR(45)"+
+			");";
+			var sql3 = "CREATE TABLE cliente("+
+				"id INT,"+
+				"fator_risco VARCHAR(45),"+
+				"renda_mensal DOUBLE,"+
+				"FOREIGN KEY (pessoa_id) REFERENCES pessoa(id),"+
+			");";
+			var slq4 = "CREATE TABLE reserva("+
+				"id INT,"+
+				"saldo DOUBLE,"+
+				"taxa DOUBLE,"+
+				"reservacol VARCHAR(45),"+
+				"FOREIGN KEY (conta_id) REFERENCES conta(id)"+
+			");";
+			var sql5 = "CREATE TABLE movimentacao_reserva("+
+				"id INT,"+
+				"dt_movimentacao DATETIME,"+
+				"valor DOUBLE,"+
+				"CHECK (tipo_movimentacao IN ('entrada', 'saida')),"+
+				"FOREIGN KEY (reserva_id) REFERENCES reserva(id)"+
+				");";
+			var sql6 = "CREATE TABLE categoria_cartao("+
+				"id INT,"+
+				"descricao VARCHAR(45)"+
+			");";
+			var sql7 = "CREATE TABLE cartao_credito("+
+				"id INT,"+
+				"dt_fechamento VARCHAR(45),"+
+				"FOREIGN KEY (conta_id) REFERENCES conta(id),"+
+				"FOREIGN KEY (categoria_cartao_id) REFERENCES categoria_cartao(id),"+
+				"limite_credito DOUBLE"+			
+			");";
+			var sql8 = "CREATE TABLE movimentacao_conta("+
+				"id INT,"+
+				"valor DOUBLE,"+
+				"dt_movimentacao DATETIME,"+
+				"CHECK (tipo_movimentacao IN ('entrada', 'saida')),"+
+				"FOREIGN KEY (conta_id) REFERENCES conta(id)"+
+			");";
+			var sql9 = "CREATE TABLE bandeira_cartao("+
+				"id INT,"+
+				"descricao VARCHAR(45)"+
+			");";
+			var sql10 = "CREATE TABLE cartao_transacao("+
+				"id INT,"+
+				"numero_cartao VARCHAR(20),"+
+				"cvc VARCHAR(5),"+
+				"FOREIGN KEY (cartao_id) REFERENCES cartao_credito(id)"+
+				"CHECK (tipo_cartao IN ('fisico', 'virtual')),"+
+				"nome_cartao VARCHAR 255"+
+				"CHECK (tipo_transacao IN ('credito', 'debito')),"+
+				"is_internacional TINYINT,"+
+				"FOREIGN KEY (bandeira_cartao_id) REFERENCES bandeira_cartao(id)"
+				+")";
+			var sql11 = "CREATE TABLE movimentacao_cartao("+
+				"id INT,"+
+				"dt_movimentacao DATETIME,"+
+				"valor DOUBLE,"+
+				"FOREIGN KEY (cartao_transacao_id) REFERENCES cartao_transacao(id),"+
+				"tipo_movimentacao VARCHAR(45)"+
+			");";
+			var sql12 = "CREATE TABLE corretor("+
+				"id INT,"+
+				"nome VARCHAR(45)"+
+			");";
+			var sql13 = "CREATE TABLE compra("+
+				"id INT,"+
+				"valor DOUBLE,"+
+				"quantidade_parcela INT,"+
+				"taxa_parcelamento DOUBLE,"+
+				"credor VARCHAR(45),"+
+				"FOREIGN KEY (corretor_id) REFERENCES corretor(id),"+
+				"FOREIGN KEY (cartao_transacao_id) REFERENCES cartao_transacao(id),"+
+				"data_compra DATETIME"+
+			");";
+			
 		};
 	}
 }
--- src/main/java/com/example/demo/rest/ClienteController.java
+++ src/main/java/com/example/demo/rest/ClienteController.java
@@ -1,17 +1,11 @@
 package com.example.demo.rest;
 
-import org.springframework.web.bind.annotation.RestController;
-
-import com.example.demo.entity.Cliente;
-import com.example.demo.entity.conta.Conta;
 import com.example.demo.services.Service;
 
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-
-import java.util.HashMap;
+import org.springframework.web.bind.annotation.RestController;
 
 
 
